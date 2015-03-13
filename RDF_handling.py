9669cb515a40c27600aba3eb0a02a2662d18080c
Mikael Trellet <mikael.trellet@u-psud.fr>
2015-03-13 16:25:49 +0100

    New RDF_Handler class to handle any RDF/SPARQL access

---
+++ RDF_handling.py
@@ -0,0 +1,169 @@
+from rdflib.plugins.sparql import prepareQuery
+from rdflib import Graph, Namespace, URIRef, Literal
+from rdflib.namespace import RDF, XSD
+import time
+import math
+import center_of_mass
+
+import logging
+logging.basicConfig(filename='pymol_session.log',filemode='w',level=logging.INFO)
+
+class RDF_Handler:
+    def __init__(self, db_file):
+        self.rdf_parsed = False
+        #self.main_handler = main_handler
+        self.rdf_graph = Graph()
+        self.parse_rdf(db_file)
+
+
+    def parse_rdf(self,filename):
+        """ Parse the RDF database """
+        start_time = time.time()
+
+        logging.info("Parsing of %s..." % filename)
+        self.rdf_graph.parse(filename, format="nt")
+        logging.info ("---- %s seconds ----" % str(time.time()-start_time))
+        logging.info("Number of triples: %d" % len(self.rdf_graph))
+        self.rdf_parsed = True
+
+    def query_sub_rdf(self, canvas, xlow, xhigh, ylow, yhigh):
+        """ Query the RDF graph for specific range of values made by user selection """
+        
+        query = 'SELECT ?id WHERE { ?point rdf:type my:point . ?point my:Y_type "'+str(canvas.y_query_type)+'" . ?point my:Y_value ?y . FILTER (?y > '+str(ylow)+' && ?y < '+str(yhigh)+') . ?point my:X_type "'+str(canvas.x_query_type)+'" . ?point my:X_value ?x . FILTER (?x > '+str(xlow)+' && ?x < '+str(xhigh)+') . ?point my:represent ?mod . ?mod my:model_id ?id .}'
+        logging.info("QUERY: \n%s" % query)
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres = self.rdf_graph.query(q)
+
+        logging.info("Number of queried entities: %d " % len(qres))
+
+        models = set()
+        for row in qres:
+            models.add(int(row[0]))
+
+        return models
+
+    def query_rdf(self, x_query_type, y_query_type, scale):
+        """ Query the RDF graph to build complete plot """
+        
+        query = 'SELECT ?x ?y ?id WHERE { ?point rdf:type my:point . ?point my:Y_type "'+y_query_type+'" . ?point my:Y_value ?y . ?point my:X_type "'+x_query_type+'" . ?point my:X_value ?x . ?point my:represent ?mod . ?mod my:'+scale+'_id ?id .}'
+        logging.info("QUERY: \n%s" % query)
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres= self.rdf_graph.query(q)
+        
+        logging.info("Number of queried entities: %d " % len(qres))
+
+        return qres
+
+    def get_analyses(self, scale):
+    	""" Get which analyses already exist for specific scale """
+
+    	query = 'SELECT DISTINCT ?x_type ?y_type WHERE { ?point my:X_type ?x_type . ?point my:Y_type ?y_type . ?point my:represent ?ind . ?ind rdf:type ?type . ?type rdfs:subClassOf* my:'+scale+' .}'
+        logging.info("QUERY: \n%s" % query)
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres = self.rdf_graph.query(q)
+
+        logging.info("Number of queried entities: %d " % len(qres))
+
+        return qres
+
+    def add_distance_points(self, item_selected, model_selected, scale):
+    	item_list, indiv_list = self.get_id_indiv_from_RDF(model_selected)
+
+    	if scale == "residue":
+            ref_x, ref_y, ref_z = center_of_mass.get_com("resid %s and model %04d" % (str(item_selected), model_selected))
+        elif scale == "atom":
+            ref_x, ref_y, ref_z = center_of_mass.get_com("id %s and model %04d" % (str(item_selected), model_selected))
+        logging.info("Coordinates of reference item: %f %f %f" % (ref_x, ref_y, ref_z))
+
+    	my = Namespace("http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#")
+        last_point_id = self.get_last_id("point")
+        nb_pt = 1
+        start_time = time.time()
+        for item in item_list:
+            if item != item_selected:
+                if scale == "residue":
+                    x,y,z = center_of_mass.get_com("resid %s and model %04d" % (str(item), model_selected))
+                elif scale == "atom":
+                    x,y,z = center_of_mass.get_com("id %s and model %04d" % (str(item), model_selected))
+                logging.info("Coordinates x y z : %f %f %f " % (x,y,z))
+                dist = math.sqrt(math.pow((ref_x-x),2)+math.pow((ref_y-y),2)+math.pow((ref_z-z),2))
+                logging.info("Distance: %f" % dist)
+                point = URIRef(my+"POINT_"+str(last_point_id+nb_pt))
+                self.rdf_graph.add( (point, RDF.type, my.point) )
+                self.rdf_graph.add( (point, my.X_value, Literal(item)) )
+                self.rdf_graph.add( (point, my.Y_value, Literal(dist)))
+                if scale == "residue":
+                    self.rdf_graph.add( (point, my.X_type, Literal('resid')))
+                    res = URIRef(str(indiv_list[nb_pt]))
+                    self.rdf_graph.add( (point, my.represent, res))
+                elif scale == "atom":
+                    self.rdf_graph.add( (point, my.X_type, Literal('atomid')))
+                    at = URIRef(str(indiv_list[nb_pt]))
+                    self.rdf_graph.add( (point, my.represent, at))
+                self.rdf_graph.add( (point, my.Y_type, Literal('distance')))
+                
+                nb_pt += 1
+
+        logging.info("---- %s seconds ----" % str(time.time()-start_time))
+
+
+    def get_id_indiv_from_RDF(self, model):
+        query = 'SELECT ?res ?resid WHERE{ ?res my:residue_id ?resid . ?res my:belongs_to+ my:MODEL_'+str(model)+' .} ORDER BY ?resid'
+        logging.info("QUERY: \n%s" % query)
+
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres = self.rdf_graph.query(q)
+
+        logging.info("Number of residues in specified model: %d " % len(qres))
+
+        id_list = []
+        indiv_list = []
+
+        for row in qres:
+            id_list.append(int(row[1]))
+            indiv_list.append(row[0])
+            logging.info("indiv: %s / id: %d" % (indiv_list[-1], id_list[-1]))
+
+        return id_list, indiv_list
+
+    def get_last_id(self, type):
+        query = 'SELECT ?id WHERE {?id rdf:type my:'+type+' .}'
+        logging.debug("QUERY: \n%s" % query)
+
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres = self.rdf_graph.query(q)
+
+        list_id = []
+        import re
+        p = re.compile("POINT_(.*)")
+        for row in qres:
+            tmp = p.search(row[0])
+            list_id.append(int(tmp.groups()[0]))
+
+        list_id.sort()
+        return list_id[-1]
+
+    def get_mini_maxi_values(self, xtype, ytype, scale):
+        """ Get minimum and maximum for x and y values from POINT individuals """
+
+        query = 'SELECT (MIN(?x) AS ?xmin) (MAX(?x) AS ?xmax) (MIN(?y) AS ?ymin) (MAX(?y) AS ?ymax) WHERE { ?point my:X_value ?x . ?point my:Y_value ?y . ?point my:X_type "'+xtype+'" . ?point my:Y_type "'+ytype+'" . ?point my:represent ?ind . ?ind rdf:type ?type . ?type rdfs:subClassOf* my:'+scale+' .}'
+        logging.info("QUERY: \n%s" % query)
+
+        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
+        qres = self.rdf_graph.query(q)
+
+        logging.info("Number of queried entities (min/max): %d " % len(qres))
+
+        res = []
+        for row in qres:
+            print row
+            for r in row:
+                if r.datatype == XSD.integer:
+                    res.append(int(r))
+                else:
+                    res.append(float(r))
+        xmin = res[0]        
+        xmax = res[1]
+        ymin = res[2]
+        ymax = res[3]
+        return xmin, xmax, ymin, ymax
--- interactive_plotting.py
+++ interactive_plotting.py
@@ -29,11 +29,10 @@ import time
 import Queue
 import threading
 import logging
-from rdflib.plugins.sparql import prepareQuery
-from rdflib import Graph, Namespace, URIRef, Literal
-from rdflib.namespace import RDF, XSD
+from RDF_handling import RDF_Handler
 import trace
 import color_by_residue
+from guppy import hpy
 
 # Parameters of logging output
 import logging
@@ -613,7 +612,7 @@ class Handler:
 
         from rdflib import Graph
 
-        self.rdf_graph = Graph()
+        self.rdf_handler = RDF_Handler("/Users/trellet/Dev/Protege_OWL/data/mod_res_pt_parsed.ntriples")
         self.queue = queue
         self.rootframe = rootframe
         self.current_canvas = self.canvas[-1]
@@ -749,6 +748,7 @@ class Handler:
         #############################################
         self.create_ids_equivalent_dict()
 
+
     ######
     # Command to select by dragging
     def onDrag(self, rect, canvas, start, end, mode):
@@ -781,7 +781,7 @@ class Handler:
             logging.info("START/END: %d:%d / %d:%d" % (start[0], start[1], end[0], end[1]))
             x_low, x_high, y_low, y_high = canvas.convertToValues(start, end)
             logging.info("Value limits: x=[%f -> %f]\ny=[%f -> %f]" % (x_low, x_high, y_low, y_high))
-            models_selected = self.query_sub_rdf(canvas, x_low, x_high, y_low, y_high)
+            models_selected = self.rdf_handler.query_sub_rdf(canvas, x_low, x_high, y_low, y_high)
             logging.info(models_selected)
             for model in models_selected:
                 self.show[model-1] = True
@@ -791,12 +791,8 @@ class Handler:
     def propose_analyses(self, scale):
         from Tkinter import BooleanVar
         self.scale = scale.lower()
-        query = 'SELECT DISTINCT ?x_type ?y_type WHERE { ?point my:X_type ?x_type . ?point my:Y_type ?y_type . ?point my:represent ?ind . ?ind rdf:type ?type . ?type rdfs:subClassOf* my:'+self.scale+' .}'
-        logging.info("QUERY: \n%s" % query)
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres = self.rdf_graph.query(q)
 
-        logging.info("Number of queried entities: %d " % len(qres))
+        qres = self.rdf_handler.get_analyses(self.scale)
 
         if len(qres) > 0:
             new_window = Tkinter.Tk()
@@ -896,7 +892,7 @@ class Handler:
             if s:
                 if k == "distance":
                     # Get list of items for the specified scale
-                    item_list, indiv_list = self.get_id_indiv_from_RDF()
+                    item_list, indiv_list = self.rdf_handler.get_id_indiv_from_RDF(self.model_selected)
                     self.rootframe.destroy()
                     rootframe = Tkinter.Tk()
                     rootframe.title(' Item of reference? ')
@@ -929,49 +925,13 @@ class Handler:
 #
 #
     def on_reference_selected_for_distance(self, evt):
-        import center_of_mass
-        item_list, indiv_list = self.get_id_indiv_from_RDF()
         if evt is not None:
             w = evt.widget
             ref = int(w.curselection()[0])
             self.item_selected = w.get(ref)
         logging.info("Item selected: %d" % self.item_selected)
-        if self.scale == "residue":
-            ref_x, ref_y, ref_z = center_of_mass.get_com("resid %s and model %04d" % (str(self.item_selected), self.model_selected))
-        elif self.scale == "atom":
-            ref_x, ref_y, ref_z = center_of_mass.get_com("id %s and model %04d" % (str(self.item_selected), self.model_selected))
-        logging.info("Coordinates of reference item: %f %f %f" % (ref_x, ref_y, ref_z))
-        # resid_list = []
-        # cmd.iterate("(name ca)","resid_list.append(resi)")
-        my = Namespace("http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#")
-        last_point_id = self.get_last_id("point")
-        nb_pt = 1
-        start_time = time.time()
-        for item in item_list:
-            if item != self.item_selected:
-                if self.scale == "residue":
-                    x,y,z = center_of_mass.get_com("resid %s and model %04d" % (str(item), self.model_selected))
-                elif self.scale == "atom":
-                    x,y,z = center_of_mass.get_com("id %s and model %04d" % (str(item), self.model_selected))
-                logging.info("Coordinates x y z : %f %f %f " % (x,y,z))
-                dist = math.sqrt(math.pow((ref_x-x),2)+math.pow((ref_y-y),2)+math.pow((ref_z-z),2))
-                logging.info("Distance: %f" % dist)
-                point = URIRef(my+"POINT_"+str(last_point_id+nb_pt))
-                self.rdf_graph.add( (point, RDF.type, my.point) )
-                self.rdf_graph.add( (point, my.X_value, Literal(item)) )
-                self.rdf_graph.add( (point, my.Y_value, Literal(dist)))
-                if self.scale == "residue":
-                    self.rdf_graph.add( (point, my.X_type, Literal('resid')))
-                    res = URIRef(str(indiv_list[nb_pt]))
-                    self.rdf_graph.add( (point, my.represent, res))
-                elif self.scale == "atom":
-                    self.rdf_graph.add( (point, my.X_type, Literal('atomid')))
-                    at = URIRef(str(indiv_list[nb_pt]))
-                    self.rdf_graph.add( (point, my.represent, at))
-                self.rdf_graph.add( (point, my.Y_type, Literal('distance')))
-                
-                nb_pt += 1
-        logging.info("---- %s seconds ----" % str(time.time()-start_time))
+        
+        self.rdf_handler.add_distance_points(self.item_selected, self.model_selected, self.scale)
         #self.rdf_graph.serialize("test.ntriples", format="nt")
         self.display_plots()
 #
@@ -994,7 +954,7 @@ class Handler:
         for k,s in self.button_dict.iteritems():
             logging.info("%s / %s : %d for %s" % (s[0], s[1], s[2].get(), self.scale))
             if s[2].get():
-                xmin, xmax, ymin, ymax = self.get_mini_maxi_values(s[0], s[1])
+                xmin, xmax, ymin, ymax = self.rdf_handler.get_mini_maxi_values(s[0], s[1], self.scale)
                 logging.info("xmin / xmax / ymin / ymax: %f %f %f %f" % (xmin, xmax, ymin, ymax))
                 self.create_window(self.rootframe, 500, 500, xmin, xmax, ymin*0.90, ymax*1.10, '', Tkinter.LEFT, s[0], s[1])
 
@@ -1048,65 +1008,6 @@ class Handler:
 
         self.current_canvas.create_line(40, 455, 300, 455, fill='black', width=1)
 
-    def get_id_indiv_from_RDF(self):
-        query = 'SELECT ?res ?resid WHERE{ ?res my:residue_id ?resid . ?res my:belongs_to+ my:MODEL_'+str(self.model_selected)+' .} ORDER BY ?resid'
-        logging.debug("QUERY: \n%s" % query)
-
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres = self.rdf_graph.query(q)
-
-        logging.info("Number of residues in specified model: %d " % len(qres))
-
-        id_list = []
-        indiv_list = []
-
-        for row in qres:
-            id_list.append(int(row[1]))
-            indiv_list.append(row[0])
-            logging.info("indiv: %s / id: %d" % (indiv_list[-1], id_list[-1]))
-
-        return id_list, indiv_list
-
-    def get_last_id(self, type):
-        query = 'SELECT ?id WHERE {?id rdf:type my:'+type+' .}'
-        logging.debug("QUERY: \n%s" % query)
-
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres = self.rdf_graph.query(q)
-
-        list_id = []
-        import re
-        p = re.compile("POINT_(.*)")
-        for row in qres:
-            tmp = p.search(row[0])
-            list_id.append(int(tmp.groups()[0]))
-
-        list_id.sort()
-        return list_id[-1]
-
-    def get_mini_maxi_values(self, xtype, ytype):
-        """ Get minimum and maximum for x and y values from POINT individuals """
-
-        query = 'SELECT (MIN(?x) AS ?xmin) (MAX(?x) AS ?xmax) (MIN(?y) AS ?ymin) (MAX(?y) AS ?ymax) WHERE { ?point my:X_value ?x . ?point my:Y_value ?y . ?point my:X_type "'+xtype+'" . ?point my:Y_type "'+ytype+'" . ?point my:represent ?ind . ?ind rdf:type ?type . ?type rdfs:subClassOf* my:'+self.scale+' .}'
-        logging.info("QUERY: \n%s" % query)
-
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres = self.rdf_graph.query(q)
-
-        logging.info("Number of queried entities (min/max): %d " % len(qres))
-
-        res = []
-        for row in qres:
-            for r in row:
-                if r.datatype == XSD.integer:
-                    res.append(int(r))
-                else:
-                    res.append(float(r))
-        xmin = res[0]        
-        xmax = res[1]
-        ymin = res[2]
-        ymax = res[3]
-        return xmin, xmax, ymin, ymax
 
     def create_ids_equivalent_dict(self):
         """ Create a dictionary of equivalent ids for each canvas created """
@@ -1454,55 +1355,14 @@ class Handler:
         cmd.delete(self.name)
         self.rootframe.destroy()
 
-    def parse_rdf(self,filename):
-        """ Parse the RDF database """
-        start_time = time.time()
-
-        logging.info("Parsing of %s..." % filename)
-        self.rdf_graph.parse(filename, format="nt")
-        logging.info ("---- %s seconds ----" % str(time.time()-start_time))
-        logging.info("Number of triples: %d" % len(self.rdf_graph))
-
-    def query_sub_rdf(self, canvas, xlow, xhigh, ylow, yhigh):
-        """ Query the RDF graph for specific range of values made by user selection """
-        
-        query = 'SELECT ?id WHERE { ?point rdf:type my:point . ?point my:Y_type "'+str(canvas.y_query_type)+'" . ?point my:Y_value ?y . FILTER (?y > '+str(ylow)+' && ?y < '+str(yhigh)+') . ?point my:X_type "'+str(canvas.x_query_type)+'" . ?point my:X_value ?x . FILTER (?x > '+str(xlow)+' && ?x < '+str(xhigh)+') . ?point my:represent ?mod . ?mod my:model_id ?id .}'
-        logging.info("QUERY: \n%s" % query)
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres = self.rdf_graph.query(q)
-
-        logging.info("Number of queried entities: %d " % len(qres))
-
-        models = set()
-        for row in qres:
-            models.add(int(row[0]))
-
-        return models
-
-    def query_rdf(self, x_query_type, y_query_type):
-        """ Query the RDF graph to build complete plot """
-        
-        query = 'SELECT ?x ?y ?id WHERE { ?point rdf:type my:point . ?point my:Y_type "'+y_query_type+'" . ?point my:Y_value ?y . ?point my:X_type "'+x_query_type+'" . ?point my:X_value ?x . ?point my:represent ?mod . ?mod my:'+self.scale+'_id ?id .}'
-        logging.info("QUERY: \n%s" % query)
-        q = prepareQuery(query, initNs = { "my": "http://www.semanticweb.org/trellet/ontologies/2015/0/VisualAnalytics#" })
-        qres= self.rdf_graph.query(q)
-        
-        logging.info("Number of queried entities: %d " % len(qres))
-
-        return qres
-
     def start(self, canvas, x_query_type, y_query_type):
         self.lock = 1
 
         canvas.x_query_type = x_query_type
         canvas.y_query_type = y_query_type
 
-        # Parse main RDF database
-        if (not self.rdf_parsed):
-            self.parse_rdf("/Users/trellet/Dev/Protege_OWL/data/mod_res_pt_parsed.ntriples")
-            self.rdf_parsed = True
         # Query RMSD points to draw first plot
-        qres = self.query_rdf(x_query_type, y_query_type)
+        qres = self.rdf_handler.query_rdf(x_query_type, y_query_type, self.scale)
 
         for row in qres:
             if int(row[2] not in self.all_models):
